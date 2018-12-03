f0654e2e0c9b30742f412c003a677cc0add5b049
jason-sheehy <jasonrheasheehy@gmail.com>
2018-12-03 16:56:57 -0600

    COUNTER

--- html/js/quoteform.js
+++ html/js/quoteform.js
@@ -1,9 +1,16 @@
 let itemCount = 0;
-const itemPlus = document.querySelectorAll('[data="cart-plus"]');
-const itemMinus = document.querySelectorAll('[data="cart-minus"]');
+const itemPlus = document.getElementById("plus");
+const itemMinus = document.getElementById('minus');
 const itemCounter = document.getElementById('item-counter');
 
-itemPlus.addEventListener("click", function(){
+itemPlus.onclick = function(){
   itemCount++;
   itemCounter.innerHTML = itemCount;
-});
+};
+
+itemMinus.onclick = function(){
+  if(itemCount>0) {
+    itemCount--;
+    itemCounter.innerHTML = itemCount;
+  }
+};
