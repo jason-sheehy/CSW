eef5c7b8482c70ab71bf9e7d6190bc3d7ff7916a
jason-sheehy <jasonrheasheehy@gmail.com>
2018-12-08 11:51:09 -0600

    FIGURING OUT SESSION STORAGE

--- html/js/quoteform.js
+++ html/js/quoteform.js
@@ -1,7 +1,32 @@
+const quoteList = {
+  items: [],
+};
+
+const addToList = (item) => {
+  alert("in here");
+  quoteList["items"].push(item);
+  jsonList = JSON.stringify(quoteList);
+  sessionStorage.setItem("list", jsonList);
+};
+
+/* const checkListForDup = (the item's name) => {
+  regex = /the item's name/;
+  return sessionStorage.getItem("list").test(regex));
+};
+*/
 let itemCount = 0;
 const itemPlus = document.getElementById("plus");
 const itemMinus = document.getElementById('minus');
 const itemCounter = document.getElementById('item-counter');
+const addItemButton = document.getElementById('add-item');
+
+addItemButton.onclick = function(){
+  let obj = {};
+  obj.itemName = "this item";
+  addToList(obj);
+  alert(sessionStorage.getItem("list"));
+
+};
 
 itemPlus.onclick = function(){
   itemCount++;
@@ -14,3 +39,6 @@ itemMinus.onclick = function(){
     itemCounter.innerHTML = itemCount;
   }
 };
+//Create object for items on list
+
+//Retrieve object of items added to list
