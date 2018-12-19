729ecf26b6f142cd66d1471ec251abd6183a2527
jason-sheehy <jasonrheasheehy@gmail.com>
2018-12-19 11:28:56 -0600

    show/hide buttons...

--- html/js/add-to-quote.js
+++ html/js/add-to-quote.js
@@ -15,10 +15,40 @@ const checkListForDup = () => {
 
 let itemCount = 0;
 let clickCount = 0;
-const itemPlus = document.getElementById("plus");
-const itemMinus = document.getElementById('minus');
-const itemCounter = document.getElementById('item-counter');
-const addItemButton = document.getElementById('add-item');
+
+const showButtons = document.getElementsByClassName('show-buttons')
+const quoteButtons = document.getElementsByClassName('quote-buttons');
+const buttonsHTML = `<div>
+  <button id="minus" class="quote-list-line quote-list-quantity-minus" data="quote-list-minus"><i class="fa fa-minus"></i></button>
+  <div class="quote-list-line quote-list-quantity-counter">
+    <span id="item-counter">1</span>
+  </div>
+  <button id="plus" class="quote-list-line quote-list-quantity-plus" data="quote-list-plus"><i class="fa fa-plus"></i></button>
+  <button id="add-item" class="quote-list-line quote-list-quantity-plus" data="quote-list-plus"><i class="fa fa-check"></i></button>
+</div>`;
+const test = () => {
+  alert("Hey there world");
+};
+
+for(let i=0; i<showButtons.length; i++) {
+  showButtons[i].onclick = function() {
+    const buttRegex = /quote-list-line/;
+    const buttonContainer = quoteButtons[i].innerHTML;
+    if(!(buttRegex.test(buttonContainer))) {
+      quoteButtons[i].innerHTML += buttonsHTML;
+      for(let j=0; j<quoteButtons.length; j++) {
+        if(j === i) {
+
+        } else {
+          if(buttRegex.test(quoteButtons[j].innerHTML)) {
+            quoteButtons[j].innerHTML = quoteButtons[j].innerHTML.replace(buttonsHTML, "");
+          }
+        }
+      }
+    }
+  };
+  
+}
 
 addItemButton.onclick = function(){
   alert(clickCount);
@@ -46,6 +76,3 @@ itemMinus.onclick = function(){
     itemCounter.innerHTML = itemCount;
   }
 };
-//Create object for items on list
-
-//Retrieve object of items added to list
