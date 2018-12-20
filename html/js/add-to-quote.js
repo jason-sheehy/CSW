4d607fb1fb4c0e6c95986e625cec1dfad31b9edc
jason-sheehy <jasonrheasheehy@gmail.com>
2018-12-20 16:40:58 -0600

    added notification, viewquote button

--- html/js/add-to-quote.js
+++ html/js/add-to-quote.js
@@ -1,40 +1,45 @@
 const quoteList = [];
 
+//receives an item, adds to quoteList, and updates sessionStorage
 const addToList = (item) => {
   quoteList.push(item);
   jsonList = JSON.stringify(quoteList);
   sessionStorage.setItem("list", jsonList);
 };
 
-const checkListForDup = () => {
-  const regex = /this\sitem/;
+//Returns true if this item is already in sessionStorage
+const checkListForDup = (itemToCheck) => {
+  const regex = new RegExp(itemToCheck);
   const theList = JSON.stringify(sessionStorage.getItem("list"));
   alert(theList);
   return regex.test(theList);
 };
 
+//Global counter variables
 let itemCount = 1;
 let clickCount = 0;
 
 const showButtons = document.getElementsByClassName('show-buttons')
 const quoteButtons = document.getElementsByClassName('quote-buttons');
-const buttonsHTML = `<div class="">
-  <button id="minus" class="quote-list-line quote-list-quantity-minus" data="quote-list-minus"><i class="fa fa-minus"></i></button>
-  <div class="quote-list-line quote-list-quantity-counter">
+//String of buttons to concatenate onto item's <div>
+const buttonsHTML = `<div class="quote-buttons-container">
+  <button id="minus" class="quote-buttons-line"><i class="fa fa-minus"></i></button>
+  <div class="quote-buttons-line quote-list-quantity-counter">
     <span id="item-counter" class="item-counter">1</span>
   </div>
-  <button id="plus" class="quote-list-line quote-list-quantity-plus" data="quote-list-plus"><i class="fa fa-plus"></i></button>
-  <button id="add-item" class="margin-eight-bottom quote-list-line quote-list-quantity-plus" data="quote-list-plus"><i class="fa fa-check"></i></button>
+  <button id="plus" class="quote-buttons-line"><i class="fa fa-plus"></i></button>
+  <button id="add-item" class="quote-buttons-line"><i class="fa fa-check"></i></button>
 </div>`;
+const addedNotification = `<div id="added" style="background-color:#71eeb8; padding:10px;"><i class="fa fa-check"></i>Added to quote</div>`;
+const addToQuoteButton = `<a class="show-buttons btn btn-small btn-rounded btn-transparent-dark-gray">Add to quote<i class="fa fa-arrow-right"></i></a>`;
+const viewQuoteButton = `<a class="btn btn-small btn-rounded btn-deep-pink margin-10px-bottom">View Quote List<i class="fa fa-arrow-right"></i></a>`;
 const test = () => {
   alert("Hey there world");
 };
-const showHideButtons = () => {
-  let buttonArea = quoteButtons[1].innerHTML;
-  buttonArea += buttonsHTML;
-};
+
+//Listen for all clicks with conditional code depending on what is clicked
 document.addEventListener("click", function(event) {
-  let buttRegex = /quote-list-line/;
+  let buttRegex = /quote-buttons-line/;
   let targetParent = event.target.parentElement;
   if(event.target.matches('.show-buttons') && !(buttRegex.test(targetParent.innerHTML))) {
     targetParent.innerHTML += buttonsHTML;
@@ -45,10 +50,11 @@ document.addEventListener("click", function(event) {
       }
       itemCount = 1;
       if(buttRegex.test(i.innerHTML) && i !== targetParent) {
-
         i.innerHTML = i.innerHTML.replace(buttonsHTML, "");
       }
     }
+
+    //Cache button elements and make them functional
     let itemMinus = document.getElementById('minus');
     let itemCounter = document.getElementById('item-counter');
     let addItemButton = document.getElementById("add-item");
@@ -56,7 +62,6 @@ document.addEventListener("click", function(event) {
     itemMinus.onclick = function(){
       if(itemCount>0) {
         itemCount--;
-
         itemCounter.innerHTML = itemCount;
       }
     };
@@ -64,21 +69,29 @@ document.addEventListener("click", function(event) {
       itemCount++;
       itemCounter.innerHTML = itemCount;
     };
+    addItemButton.onclick = function(){
+      let findCategory = document.getElementsByClassName('item-category');
+      let itemCategory = findCategory[0].id;
+      let itemName = targetParent.firstElementChild.innerHTML;
+      let itemToAdd = `${itemName} ${itemCategory}`;
+      if(clickCount > 0) {
+        if(checkListForDup(itemToAdd)){
+          return;
+        }
+      }
+      let obj = {};
+      obj.itemName = itemToAdd;
+      obj.quantity = Number(itemCounter.innerHTML);
+      addToList(obj);
+      clickCount++;
+      alert(sessionStorage.getItem("list"));
+      itemCounter.innerHTML = 1;
+      targetParent.innerHTML = targetParent.innerHTML.replace(addToQuoteButton, "");
+      targetParent.innerHTML = targetParent.innerHTML.replace(buttonsHTML, addedNotification);
+      window.setTimeout(closeAddedNotification, 1000);
+      function closeAddedNotification() {
+        targetParent.innerHTML = targetParent.innerHTML.replace(addedNotification, viewQuoteButton);
+      }
+    };
   }
 }, false);
-
-
-addItemButton.onclick = function(){
-  alert(clickCount);
-  if(clickCount > 0) {
-    if(checkListForDup()){
-      return;
-    }
-  }
-  let obj = {};
-  obj.itemName = "this item";
-  obj.quantity = Number(itemCounter.innerHTML);
-  addToList(obj);
-  clickCount++;
-  alert(sessionStorage.getItem("list"));
-};
